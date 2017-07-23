7a704660b449d93e86c9f0908976c4a73faba6b1
Shmuel Maruani <shmuel@limitlessv.com>
2017-07-23 11:42:43 -0400

    Installing the Python Interpreter via anaconda

---
+++ python/an.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE}")";
+
+wget https://repo.continuum.io/archive/Anaconda3-4.4.0-MacOSX-x86_64.sh
+
+# Install in batch mode
+bash Anaconda3-4.4.0-MacOSX-x86_64.sh -b
+
+# Install in a batch mode required to add path
+export PATH="~/anaconda3/bin:$PATH"
+
+# add path to ~/.path
+echo # For anaconda >> ~/.path
+echo export PATH='~/anaconda3/bin:$PATH' >> ~/.path
+
+
