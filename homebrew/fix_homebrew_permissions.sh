b785695d080728f4a52a050df9d9d06e7c934dcf
Shmuel Maruani <limitlessv@users.noreply.github.com>
2018-05-10 15:20:06 -0700

    Update fix_homebrew_permissions.sh

--- homebrew/fix_homebrew_permissions.sh
+++ homebrew/fix_homebrew_permissions.sh
@@ -8,11 +8,11 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # https://stackoverflow.com/questions/16432071/how-to-fix-homebrew-permissions/16450503#16450503
 
-sudo chgrp -R admin /usr/local
-sudo chmod -R g+w /usr/local
+sudo chgrp -R -L -f -v admin /usr/local
+sudo chmod -R  -L -v g+w /usr/local
 
-sudo chgrp -R admin /Library/Caches/Homebrew
-sudo chmod -R g+w /Library/Caches/Homebrew
+sudo chgrp -R -L -f -v admin /Library/Caches/Homebrew
+sudo chmod -R -L -v g+w /Library/Caches/Homebrew
 
-sudo chgrp -R admin /opt/homebrew-cask
-sudo chmod -R g+w /opt/homebrew-cask
+sudo chgrp -R -L -f -v admin /opt/homebrew-cask
+sudo chmod -R -L -v g+w /opt/homebrew-cask
